f80f975392cfcd6f10e16c0f52c575cae7974efa
Eyal Kapah <eykapah@microsoft.com>
2020-10-06 11:03:36 +0300

    add ticker

---
+++ YahooFinance.Runner/Models/Ticker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahooFinance.Runner.Models
+{
+    public class Ticker
+    {
+        public string Symbol { get; }
+
+        public Ticker(string symbol)
+        {
+            Symbol = symbol;
+        }
+    }
+}
