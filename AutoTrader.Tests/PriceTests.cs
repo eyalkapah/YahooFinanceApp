e809fc5cafe0e5aa4099707e85eb1bf6759f05ec
Eyal Kapah <eykapah@microsoft.com>
2020-10-07 23:00:29 +0300

    add test project

---
+++ AutoTrader.Tests/PriceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutoTrader.Tests
+{
+    [TestClass]
+    public class PriceTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
